ccc71799f2a8c8a19d6fcab2cea18529c09ef575
YASSINE <contact@yassine.fun>
2024-02-27 07:40:27 -0800

    100-textme.rb, Update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,28 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.+?)\] \[to:(.+?)\] \[flags:(.+?)\]/).map(&:join).join(',')
+
+if ARGV.length != 1
+  puts "Usage: #{$0} 'log_entry'"
+  exit
+end
+
+log_entry = ARGV[0]
+
+# Regular expression to match the required fields
+pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+
+match_data = pattern.match(log_entry)
+
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+
+  # Ensure '+' signs are not removed in output
+  sender.gsub!(/^\+/, '')
+  receiver.gsub!(/^\+/, '')
+
+  # Print the formatted output
+  puts "#{sender},#{receiver},#{flags}"
+else
+  puts "No match found"
+end
