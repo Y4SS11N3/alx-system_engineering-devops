240b33ff3ef4b4361adfeb975203155dd7813b5e
YASSINE <contact@yassine.fun>
2024-04-19 10:14:12 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Install Flask package with version 2.1.0 using pip3
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
