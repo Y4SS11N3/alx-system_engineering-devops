fa3b0daf377a3d54ac860b7cb8f462a287ac7b2f
YASSINE <contact@yassine.fun>
2024-04-29 00:40:33 -0700

    0x0F-load_balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,46 @@
+# 2-puppet_custom_http_response_header.pp
+
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create custom HTTP header configuration
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => file,
+  content => 'add_header X-Served-By $hostname;',
+  notify  => Service['nginx'],
+}
+
+# Configure Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      root /var/www/html;
+      index index.html;
+
+      server_name _;
+
+      include /etc/nginx/conf.d/custom_header.conf;
+    }
+  ",
+  notify  => Service['nginx'],
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Create index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
