88a05256725c831c9630c637df83c5fffa0144d3
YASSINE <contact@yassine.fun>
2024-05-19 23:31:54 -0700

    Export employee TODO data to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that exports data in CSV format using the provided REST API.
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    employee_name = response.json().get("username")
+
+    todos_url = f"https://jsonplaceholder.typicode.com/todos"
+    todos_response = requests.get(todos_url, params={"userId": employee_id})
+    todos = todos_response.json()
+
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode="w", newline="") as csv_file:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+
+        for task in todos:
+            writer.writerow({
+                "USER_ID": employee_id,
+                "USERNAME": employee_name,
+                "TASK_COMPLETED_STATUS": task.get("completed"),
+                "TASK_TITLE": task.get("title")
+            })
