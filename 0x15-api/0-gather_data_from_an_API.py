94faaeb9e11288ebb084fe2a3a0d96d748c70e3f
YASSINE <contact@yassine.fun>
2024-05-20 04:29:30 -0700

    Gather employee TODO data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(url)
-    employee_name = response.json().get("name")
+    employee_data = response.json()
+    employee_name = employee_data.get("name")
 
     todos_url = f"https://jsonplaceholder.typicode.com/todos"
     todos_response = requests.get(todos_url, params={"userId": employee_id})
@@ -22,7 +23,9 @@ if __name__ == "__main__":
     completed_tasks = [todo for todo in todos if todo.get("completed")]
     done_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks"
-          f"({done_tasks}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks"
+        f"({done_tasks}/{total_tasks}):"
+    )
     for task in completed_tasks:
         print(f"\t {task.get('title')}")
