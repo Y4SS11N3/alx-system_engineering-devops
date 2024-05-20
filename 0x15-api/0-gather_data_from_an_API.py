0ee398ea367e8f5b2d2a03e2a985f01099629291
YASSINE <contact@yassine.fun>
2024-05-20 04:25:58 -0700

    Gather employee TODO data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Script that returns information about an employee's TODO list progress
 using the provided REST API.
@@ -21,6 +22,7 @@ if __name__ == "__main__":
     completed_tasks = [todo for todo in todos if todo.get("completed")]
     done_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({done_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task.get('title')}")
