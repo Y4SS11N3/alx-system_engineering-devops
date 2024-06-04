e1465bdb28af3cb7459f7e5f424472a6bf3b8ce0
YASSINE <contact@yassine.fun>
2024-06-04 13:44:49 -0700

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,13 +10,20 @@ def top_ten(subreddit):
     Queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit.
     """
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
+        return
+
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "MyBot/0.0.1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
+        try:
+            for post in data["data"]["children"]:
+                print(post["data"]["title"])
+        except KeyError:
+            print(None)
     else:
         print(None)
