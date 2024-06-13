40ac1053b951de56dfaaf2e613e773bea46776fc
YASSINE <contact@yassine.fun>
2024-06-13 09:32:09 -0700

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # This manifest changes the OS configuration to allow the "holberton" user to log in and open files without error
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard nofile/d" /etc/security/limits.conf; sed -i "/holberton soft nofile/d" /etc/security/limits.conf; echo "holberton hard nofile 4096" >> /etc/security/limits.conf; echo "holberton soft nofile 1024" >> /etc/security/limits.conf',
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard nofile/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft nofile/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
